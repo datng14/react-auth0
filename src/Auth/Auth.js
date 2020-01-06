744c8013918d484791ae80f44d675ca95584fc32
Datng <nguyendat1411@gmail.com>
2020-01-06 21:23:00 +0700

    add logout

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -3,6 +3,8 @@ import auth0 from "auth0-js";
 export default class Auth {
   constructor(history) {
     this.history = history;
+    this.userProfile = null;
+
     this.auth0 = new auth0.WebAuth({
       domain: process.env.REACT_APP_AUTH0_DOMAIN,
       clientID: process.env.REACT_APP_AUTH0_CLIENTID,
@@ -43,11 +45,34 @@ export default class Auth {
     localStorage.removeItem("access_token");
     localStorage.removeItem("is_token");
     localStorage.removeItem("expires_at");
-    this.history.push("/");
+    this.auth0.logout({
+      clientID: process.env.REACT_APP_AUTH0_CLIENTID,
+      returnTo: "http://localhost:3000"
+    })
   };
 
-  isAuthenticated() {
+  isAuthenticated = () =>  {
     const expiresAt = JSON.parse(localStorage.getItem("expires_at"));
     return new Date().getTime() < expiresAt;
+  }
+
+  getAccessToken = () => {
+    const accessToken = localStorage.getItem('access_token');
+    if (!accessToken) {
+      throw new Error('No access token found');
+    }
+    return accessToken;
+  }
+
+  getProfile = cback => {
+    if (this.userProfile) {
+      return cback(this.userProfile)
+    }
+    this.auth0.client.userInfo(this.getAccessToken(), (err, profile) => {
+      if (profile) {
+        this.userProfile = profile;
+      }
+      cback(profile, err);
+    })
   }
 }
